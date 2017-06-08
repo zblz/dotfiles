eb9b58cece71dae21be5c6ab651af45cd0b74cdd
Víctor Zabalza <vzabalza@gmail.com>
2017-06-08 10:41:20 +0100

    add container argument to k8slogs

--- tag-fish/config/fish/functions/k8slogs.fish
+++ tag-fish/config/fish/functions/k8slogs.fish
@@ -1,7 +1,11 @@
-function k8slogs --description 'Logs for a pod' --argument namespace
+function k8slogs --description 'Logs for a pod' --argument namespace --argument container
   if test -z $namespace
     echo "Missing argument NAMESPACE"
     return 1
+  end
+
+  if test -z $container
+      set -l container "-c $container"
   end
 
   kubectl get pods -o=custom-columns=NAME:.metadata.name --namespace $namespace | \
@@ -9,5 +13,5 @@ function k8slogs --description 'Logs for a pod' --argument namespace
     fzy | \
     read -l pod
   echo $pod
-  kubectl logs -f $pod --namespace $namespace --since 12h
+  kubectl logs -f $pod --namespace $namespace $container --since 12h
 end
