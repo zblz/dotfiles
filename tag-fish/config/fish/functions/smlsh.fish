624a0e618f4b9d80a770c21b2512bd6ad38e9e28
Víctor Zabalza <vzabalza@gmail.com>
2017-06-09 13:57:03 +0100

    fish: update smlsh

--- tag-fish/config/fish/functions/smlsh.fish
+++ tag-fish/config/fish/functions/smlsh.fish
@@ -1,6 +1,16 @@
-function smlsh --description 'Connect to a SML instance'
-  sml projects -v | tail -n +2 | fzy | read -l project_result
-  set -l project_id (echo $project_result | cut -d ' ' -f 1)
-  sml servers $project_id | fzy | read -l server_result
-  sml shell $project_id $server_result
+function smlsh -a query -d 'SSH to a SherlockML server'
+  sml projects -v | tail -n +2 | fzy -q "$query" -p 'project> ' | \
+    read -l project
+  set -l project_id (echo $project | cut -d ' ' -f 1)
+  set -l servers (sml server list $project_id)
+  if test (count $servers) -eq 0
+    echo 'No servers in project'
+    return
+  else if test (count $servers) -eq 1
+    set server $servers[1]
+  else
+    echo -e (string join '\n' $servers) | fzy -p 'server> ' | \
+      read -l server
+  end
+  sml shell $project_id $server -A
 end
