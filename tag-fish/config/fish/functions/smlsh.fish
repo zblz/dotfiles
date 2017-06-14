ff3fb5868a647d51d7435363f0e851b99fd27ce9
Víctor Zabalza <vzabalza@gmail.com>
2017-06-14 17:54:37 +0100

    fish: update smlsh

--- tag-fish/config/fish/functions/smlsh.fish
+++ tag-fish/config/fish/functions/smlsh.fish
@@ -1,16 +1,13 @@
-function smlsh -a query -d 'SSH to a SherlockML server'
-  sml projects -v | tail -n +2 | fzy -q "$query" -p 'project> ' | \
+function smlsh -a project_query -d 'SSH to a SherlockML server'
+  sml projects -v | tail -n +2 | fzy -q "$project_query" -p 'project > ' | \
     read -l project
   set -l project_id (echo $project | cut -d ' ' -f 1)
   set -l servers (sml server list $project_id)
-  if test (count $servers) -eq 0
-    echo 'No servers in project'
-    return
-  else if test (count $servers) -eq 1
+  if test (count $servers) -eq 1 # If there is only one server, use that
     set server $servers[1]
   else
-    echo -e (string join '\n' $servers) | fzy -p 'server> ' | \
-      read -l server
+    echo -e (string join '\n' $servers) | fzy -p "server > " | read server
   end
-  sml shell $project_id $server -A
+
+  sml shell $project_id $server
 end
