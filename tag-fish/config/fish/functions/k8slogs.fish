3e1a26e9d9297829c3d9d63728813ea02d4b4424
Víctor Zabalza <vzabalza@gmail.com>
2017-07-12 09:11:12 +0100

    fish: add namespace selector to k8slogs

--- tag-fish/config/fish/functions/k8slogs.fish
+++ tag-fish/config/fish/functions/k8slogs.fish
@@ -1,17 +1,11 @@
-function k8slogs --description 'Logs for a pod' --argument namespace --argument container
-  if test -z $namespace
-    echo "Missing argument NAMESPACE"
-    return 1
-  end
-
+function k8slogs --description 'Logs for a pod' --argument container
   if test -z $container
       set -l container "-c $container"
   end
 
-  kubectl get pods -o=custom-columns=NAME:.metadata.name --namespace $namespace | \
-    tail -n+2 | \
-    fzy | \
-    read -l pod
-  echo $pod
+  kubectl get namespaces --no-headers  | fzy -p 'namespace > ' | \
+    awk '{print $1}' | read -l namespace
+  kubectl get pods --no-headers --namespace $namespace | fzy -p 'pod > '| \
+    awk '{print $1}' | read -l pod
   kubectl logs -f $pod --namespace $namespace $container --since 12h
 end
