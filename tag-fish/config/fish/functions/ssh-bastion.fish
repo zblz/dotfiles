61b9c255d7c47b4d53d6441614f511f7b5361aeb
Víctor Zabalza <vzabalza@gmail.com>
2019-04-02 10:07:16 +0100

    add user to ssh-bastion

--- tag-fish/config/fish/functions/ssh-bastion.fish
+++ tag-fish/config/fish/functions/ssh-bastion.fish
@@ -1,4 +1,4 @@
 function ssh-bastion
 	set-aws-creds $argv
-	ssh (~/src/bitbucket.org/theasi/infra-deploy-aws/scripts/get-bastion-ip.sh);
+	ssh victor@(~/src/bitbucket.org/theasi/infra-deploy-aws/scripts/get-bastion-ip.sh);
 end
