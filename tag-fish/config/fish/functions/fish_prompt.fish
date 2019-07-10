f6538b73b67ea2b678703fcf4fe410c0b5abdf75
Víctor Zabalza <vzabalza@gmail.com>
2019-07-10 16:37:43 +0000

    fish: Use server name in prompt in Faculty platform

--- tag-fish/config/fish/functions/fish_prompt.fish
+++ tag-fish/config/fish/functions/fish_prompt.fish
@@ -67,7 +67,7 @@ function fish_prompt --description 'Write out the prompt'
 
     switch $__fish_prompt_hostname
         case "cube-*"
-            set -gx __fish_prompt_hostname cube-dev
+            set -gx __fish_prompt_hostname $FACULTY_SERVER_NAME
     end
 
 	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "> "
