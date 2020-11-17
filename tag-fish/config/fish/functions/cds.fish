e672fafa1e364b66187cb0187a799b7fa178c5ab
Víctor Zabalza <vzabalza@gmail.com>
2020-11-17 13:27:26 +0000

    fish: Use git-find-repos for cds

--- tag-fish/config/fish/functions/cds.fish
+++ tag-fish/config/fish/functions/cds.fish
@@ -2,9 +2,9 @@
 
 function cds -d 'Change directory to a source code repository'
     if count $argv > /dev/null
-        git ls-repos | grep -i $argv[1] | read -g result
+        git find-repos $HOME/src | grep -i $argv[1] | read -g result
     else
-        git ls-repos | fzy | read -g result
+        git find-repos $HOME/src | fzy | read -g result
     end
     cd ~/src/$result
 end
