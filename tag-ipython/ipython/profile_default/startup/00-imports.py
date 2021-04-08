8ac1b0d202b7c41cd2af7da350a31eba92183eac
Víctor Zabalza <vzabalza@gmail.com>
2021-04-08 21:15:56 +0100

    Only import numpy into ipython

--- tag-ipython/ipython/profile_default/startup/00-imports.py
+++ tag-ipython/ipython/profile_default/startup/00-imports.py
@@ -2,17 +2,3 @@ try:
     import numpy as np
 except ImportError:
     pass
-
-try:
-    import astropy.units as u
-except ImportError:
-    pass
-
-try:
-    # matplotlib
-    import matplotlib as mpl
-    import matplotlib.pyplot as plt
-    # plt.style.use('seaborn-whitegrid')
-    # plt.style.use('seaborn-deep')
-except (ImportError, RuntimeError):
-    pass
