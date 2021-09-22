b38e4cfd7829a54e41ba59b9963c14146e94ffd4
Víctor Zabalza <vzabalza@gmail.com>
2021-09-22 08:33:18 +0100

    fish: add current python version to right prompt

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -28,13 +28,6 @@ switch (uname)
             alias workon 'conda activate'
             complete -f -c workon -a '(__fish_conda_envs)'
             workon Python3
-        else
-            # Set up virtualfish
-            set -x WORKON_HOME "$HOME/virtualenvs"
-            set -x PROJECT_HOME "$HOME/projects"
-            set -g VIRTUALFISH_HOME $WORKON_HOME
-            set -g VIRTUALFISH_COMPAT_ALIASES "True"
-            eval (python -m virtualfish compat_aliases auto_activation global_requirements)
         end
 end
 
--- tag-fish/config/fish/functions/fish_prompt.fish
+++ tag-fish/config/fish/functions/fish_prompt.fish
@@ -75,6 +75,6 @@ end
 
 function fish_right_prompt -d "Write out the right prompt"
     if set -q VIRTUAL_ENV
-		echo -n -s (set_color blue) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+		echo -n -s (set_color brblue) "(" (basename "$VIRTUAL_ENV") "|" (python --version | cut -f 2 -d ' ') ")" (set_color normal) " "
 	end
 end
