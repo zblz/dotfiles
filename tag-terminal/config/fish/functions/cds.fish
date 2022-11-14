049ab52de716ed706e5147a92319cc013fe45286
Víctor Zabalza <vzabalza@gmail.com>
2022-11-14 22:24:54 +0000

    fish: use fzf for cds

--- tag-terminal/config/fish/functions/cds.fish
+++ tag-terminal/config/fish/functions/cds.fish
@@ -4,7 +4,7 @@ function cds -d 'Change directory to a source code repository'
     if count $argv > /dev/null
         git find-repos $HOME/src | grep -i $argv[1] | read -g result
     else
-        git find-repos $HOME/src | fzy | read -g result
+        git find-repos $HOME/src | fzf | read -g result
     end
     cd ~/src/$result
 end
