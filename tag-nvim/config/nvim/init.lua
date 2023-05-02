3847734ac362199b25ef171fffc95e0c78636401
Víctor Zabalza <vzabalza@gmail.com>
2023-05-02 13:46:02 +0100

    nvim: s/sumneko_lua/lua_ls/

--- tag-nvim/config/nvim/init.lua
+++ tag-nvim/config/nvim/init.lua
@@ -337,7 +337,7 @@ end
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Enable the following language servers
-local servers = { 'pyright', 'metals', 'yamlls', 'sumneko_lua', 'bashls' }
+local servers = { 'pyright', 'metals', 'yamlls', 'lua_ls', 'bashls' }
 for _, lsp in ipairs(servers) do
         nvim_lsp[lsp].setup {
                 on_attach = on_attach,
