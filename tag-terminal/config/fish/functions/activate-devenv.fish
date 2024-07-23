dc630e37a183b5a8417fa43120a46411d398d609
Víctor Zabalza <vzabalza@gmail.com>
2024-07-23 11:49:34 +0100

    fish: activate-devenv change priorities

--- tag-terminal/config/fish/functions/activate-devenv.fish
+++ tag-terminal/config/fish/functions/activate-devenv.fish
@@ -1,11 +1,14 @@
 function activate-devenv
   set -l poetry_venv (poetry env info --path)
-  if test -n "$poetry_venv"
-    echo "Activating virtual environment from poetry"
-    source $poetry_venv/bin/activate.fish
-  else if test -d venv
+  if test -d venv
     echo "Activating virtual environment from ./venv"
     source venv/bin/activate.fish
+  else if test -d .venv
+    echo "Activating virtual environment from ./.venv"
+    source .venv/bin/activate.fish
+  else if test -n "$poetry_venv"
+    echo "Activating virtual environment from poetry"
+    source $poetry_venv/bin/activate.fish
   else
     echo "No virtual environment found"
   end
